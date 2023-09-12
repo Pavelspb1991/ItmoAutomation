859ea737a274a4aec4b88e07aa6d6dc1e35338e7
Pavel <sn0oze@mail.ru>
2023-09-12 23:53:54 +0300

    second commit

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -3,14 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 
-def transion():
+def test_visit():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
 
     try:
-        driver.find_element(By.CSS_SELECTOR, "user > name")
+        driver.find_element(By.XPATH, "//*[@id='user-name']")
+        driver.find_element(By.XPATH, "//*[@id='password']")
+        driver.find_element(By.XPATH, "//*[@id='login-button']")
     except NoSuchElementException:
         assert False
-    assert print("Элементы найдены")
+    assert True
+    print("Элементы найдены")
+
 
 
